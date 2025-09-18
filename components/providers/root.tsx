1d84e8da2a388c96e063477ca68e7f33c16eb4cb
Arthur Mendes <thumendess@gmail.com>
2025-09-18 10:57:19 -0300

    Adicionando dialoger

--- components/providers/root.tsx
+++ components/providers/root.tsx
@@ -4,6 +4,7 @@ import { queryClient } from "@/lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "../ui/sonner";
 import { ThemeProvider } from "next-themes";
+import { Dialoger } from "../dialog";
 type RootProvidersProps = {
   children: React.ReactNode;
 };
@@ -13,6 +14,7 @@ export function RootProviders({ children }: RootProvidersProps) {
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <QueryClientProvider client={queryClient}>
         <Toaster richColors />
+        <Dialoger />
         {children}
       </QueryClientProvider>
     </ThemeProvider>
